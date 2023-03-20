1117d1522706e61ce806965ebfa331595c88fd44
xijia007 <jia.xi1@northeastern.edu>
2023-03-20 11:52:26 -0700

    a7 Redux sp23

--- src/tuiter/profile/profile-edit-item.js
+++ src/tuiter/profile/profile-edit-item.js
@@ -66,28 +66,31 @@ const ProfileEditItem = () => {
       {/* <form onSubmit={handleSaveSubmit}> */}
       <div id="top" className="d-flex justify-content-between">
         <div className="d-flex justify-content-between">
-          <div id="Xbuttion" style={{ marginRight: "20px" }}>
+          <div
+            id="Xbuttion"
+            to="/tuiter/profile"
+            style={{ marginRight: "20px" }}
+          >
             {/* <a href="/tuiter/profile"> */}
-
-            <button
-              className="btn"
-              style={{
-                fontSize: "15px",
-              }}
-            >
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="25"
-                height="25"
-                fill="currentColor"
-                class="bi bi-x"
-                viewBox="0 0 16 16"
+            <Link to="/tuiter/profile">
+              <button
+                className="btn"
+                style={{
+                  fontSize: "15px",
+                }}
               >
-                <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
-              </svg>
-            </button>
-
-            {/* </a> */}
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="25"
+                  height="25"
+                  fill="currentColor"
+                  class="bi bi-x"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z" />
+                </svg>
+              </button>
+            </Link>
           </div>
           <div
             style={{
