bffb7936e91599a1834b29ef1e3ef92892b76257
xijia007 <jia.xi1@northeastern.edu>
2023-04-07 11:53:38 -0700

    a9 Mongo Spring23

--- src/App.js
+++ src/App.js
@@ -1,33 +1,3 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-// function App() {
-//   return <h1>Hello World!</h1>;
-// }
-// export default App;
-
 import Labs from "./labs";
 import HelloWorld from "./labs/a6/hello-world";
 import Tuiter from "./tuiter";
@@ -40,11 +10,10 @@ function App() {
     <BrowserRouter>
       <div className="container">
         <Routes>
-          {/* <Route index element={<Labs />} /> */}
           <Route path="/*" element={<Labs />} />
-          {/* <Route path="/labs" element={<Labs />} /> */}
+
           <Route path="/hello" element={<HelloWorld />} />
-          {/* <Route path="/tuiter" element={<Tuiter />} /> */}
+
           <Route path="/tuiter/*" element={<Tuiter />} />
 
           {/* <Route path="/tuiter/explore" element={<ExploreComponent />} /> */}
